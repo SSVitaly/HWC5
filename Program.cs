df8ab0ae37fae8f1259f050605ba351a3e303538
SVitaly <user7_490@mail.ru>
2023-01-26 21:10:13 +0300

    Добавлено решение 36 задачи

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ Console.WriteLine();
 }
 
 //задача 34
+Console.WriteLine("Задача 34");
 Console.WriteLine("введите количество элементов в массиве:");
 int num = Convert.ToInt32(Console.ReadLine());
 int[] myArray = CreateRandomArray(num, 100, 1000);
@@ -44,4 +45,25 @@ ShowArray(myArray);
     {
       if (myArray[i] % 2 == 0) even += 1;
     }
-    Console.WriteLine($"количество четных элементов: {even}");
+Console.WriteLine($"количество четных элементов: {even}");
+
+//задача 36 
+Console.WriteLine(" ");
+Console.WriteLine("Задача 36");
+Console.WriteLine("Введите количество элементов в массиве");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первое число случайно генерируемого диапазона");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите последнее число случайно генерируемого диапазона");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myRandomArray = CreateRandomArray(num1, min, max);
+Console.WriteLine("Массив:");
+    ShowArray(myRandomArray);
+int oddNumber = 0;
+     
+    for (int i = 0; i < myRandomArray.Length; i++)
+    {
+      if (i % 2 == 0) oddNumber += myRandomArray[i];
+    }
+    Console.WriteLine($"сумма элементов на нечетных позициях, с учетом того что позиции нумеруются с 0: {oddNumber}");
